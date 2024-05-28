61b5c257421bf3dbfc1e5da406d2d81c79720d38
rizki <rizki.nofsyiami@gmail.com>
2024-05-28 10:22:35 +0700

    init

---
+++ eslint.config.mjs
@@ -0,0 +1,12 @@
+import withNuxt from './.nuxt/eslint.config.mjs'
+
+export default withNuxt({
+  files: ['**/*.js', '**/*.vue', '**/*.ts', '**/*.tsx'],
+  rules: {
+    'no-console': 'error',
+    'no-nested-ternary': 'error',
+    'no-unneeded-ternary': 'error',
+    'semi': ['error', 'never'],
+    'quotes': ['error', 'single'],
+  }
+})
