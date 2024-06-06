5d68ecee93a109a9788a3b4ae7e16d96b0f00c4c
rizki <rizki.nofsyiami@gmail.com>
2024-06-06 09:43:16 +0700

    feat(editor) - text font style

--- components/editor/el/text/index.vue
+++ components/editor/el/text/index.vue
@@ -1,12 +1,25 @@
 <script setup lang="ts">
 import type {TextEl} from '~/components/el/text'
 
-const selectedContent =  useSelectedElement<TextEl>()
+const el = useSelectedElement<TextEl>()
 
 </script>
 
 <template>
-  <div>
-    <ui-textarea v-model="selectedContent!.props.content"/>
+  <div v-if="el">
+    <ui-textarea v-model="el.props.content"/>
+    <editor-style-font
+      class="mt-4"
+      v-model:color="el.props.styles.color"
+      v-model:fontSize="el.props.styles.fontSize"
+      v-model:fontFamily="el.props.styles.fontFamily"
+      v-model:fontWeight="el.props.styles.fontWeight"
+      @delete="[
+        delete el.props.styles.fontSize, 
+        delete el.props.styles.color, 
+        delete el.props.styles.fontFamily,
+        delete el.props.styles.fontWeight,
+      ]"
+    />
   </div>
 </template>
--- components/el/text/index.ts
+++ components/el/text/index.ts
@@ -1,5 +1,7 @@
  
 import { h } from 'vue'
+import type { Styles } from '~/types/site'
+import { generateStyle } from '~/utils/style'
 
 const as = ['p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6'] as const
 
@@ -7,6 +9,7 @@ export interface Props {
   as?: typeof as[number]
   content: string
   class?: string
+  styles: Styles
 }
 
 export interface TextEl {
@@ -18,6 +21,6 @@ export interface TextEl {
 export default (props: Props) => {
   const Tag = props.as || 'p'
   return (
-    h(Tag, { class: props.class }, props.content)
+    h(Tag, { class: [props.class, generateStyle(props.styles)] }, props.content)
   )
 }
